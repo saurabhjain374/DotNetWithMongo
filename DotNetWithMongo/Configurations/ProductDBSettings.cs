1f69d170d78c00f319e748768602ea923f09fe43
Saurabh Jain <saurabh.jain@onsolve.com>
2024-04-30 16:28:44 +0530

    project commit

---
+++ DotNetWithMongo/Configurations/ProductDBSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetWithMongo.Configurations
+{
+    public class ProductDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string ProductCollectionName { get; set; }
+    }
+}
---
+++ DotNetWithMongo/Repositories/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using DotNetWithMongo.Entities;
+
+namespace DotNetWithMongo.Repositories
+{
+    public interface IProductService
+    {
+        public Task<List<ProductDetails>> ProductListAsync();
+        public Task<ProductDetails> GetProductDetailByIdAsync(string productId);
+        public Task AddProductAsync(ProductDetails productDetails);
+        public Task UpdateProductAsync(string productId, ProductDetails productDetails);
+        public Task DeleteProductAsync(String productId);
+    }
+}
